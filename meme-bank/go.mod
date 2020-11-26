f8c97796766e7d6e21447d99a98feef4fde81900
Daniel Núñez <42550783+JumpNShootMan@users.noreply.github.com>
2020-11-26 15:17:42 -0500

    Add files via upload

---
+++ meme-bank/go.mod
@@ -0,0 +1,10 @@
+module github.com/jamilchioino/meme-bank
+
+go 1.15
+
+require (
+	github.com/go-playground/validator/v10 v10.4.1
+	github.com/gofiber/fiber/v2 v2.2.0
+	github.com/gofiber/websocket/v2 v2.0.2
+	github.com/jinzhu/gorm v1.9.16
+)
---
+++ meme-bank/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/websocket/v2"
+	"github.com/jamilchioino/meme-bank/database"
+	"github.com/jamilchioino/meme-bank/models"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+func setupRoutes(app *fiber.App) {
+
+	app.Get("/api/v1/transactions", models.GetTransactions)
+	app.Get("/api/v1/transaction/:id", models.GetTransaction)
+	app.Post("/api/v1/transaction", models.NewTransaction)
+	app.Delete("/api/v1/transaction/:id", models.DeleteTransaction)
+	app.Get("/api/v1/transactions/user/:id", models.GetTransactionsOfUser)
+
+	//////////////////////////////////////////////////////////
+	app.Get("/api/v1/users", models.GetUsers)
+	app.Get("/api/v1/user/:id", models.GetUser)
+	app.Post("/api/v1/user", models.NewUser)
+	app.Delete("/api/v1/user/:id", models.DeleteUser)
+	//////////////////////////////////////////////////////////
+	app.Post("/api/v1/login", models.LogIn)
+
+	///////////////////////////////////////////////////////////
+	app.Use("/ws", func(c *fiber.Ctx) error {
+		// IsWebSocketUpgrade returns true if the client
+		// requested upgrade to the WebSocket protocol.
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Locals("allowed", true)
+			return c.Next()
+		}
+		return fiber.ErrUpgradeRequired
+	})
+
+	app.Get("/ws/transactions", models.HandleWebsocket())
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "banks.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+	database.DBConn.AutoMigrate(&models.Transaction{})
+	database.DBConn.AutoMigrate(&models.User{})
+	fmt.Println("Database Migrated")
+}
+
+func main() {
+	app := fiber.New()
+	app.Use(logger.New())
+	app.Use(cors.New())
+	// app.Use(cors.New(cors.Config{
+	// 	AllowOrigins: "*",
+	// 	AllowHeaders: "*",
+	// }))
+
+	setupRoutes(app)
+
+	initDatabase()
+
+	app.Listen(":1996")
+
+	defer database.DBConn.Close()
+}
